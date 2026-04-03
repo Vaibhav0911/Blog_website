dd33f0267ba2df77775af324daef7fd178d9a07d
Vaibhav <chauhanvaibhav758@gmail.com>
2026-04-03 13:02:36 +0530

    initialized setup and appwrite routes

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf.js";
+
+export class AuthServices {
+   
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+           .setEndpoint(conf.appwriteUrl)
+           .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);    
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create({userId: ID.unique(), email, password, name});
+            if(userAccount){
+               this.login({email, password});  
+            }
+            else       return userAccount;
+        } catch (error) {
+            console.log("error in creating account", error);
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession({email, password});
+        } catch (error) {
+            console.log("error in login account", error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+     
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+
+}
+
+const authServices = new AuthServices();
+
+export default authServices;
